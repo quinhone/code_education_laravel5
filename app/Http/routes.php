95dd028bf154c433d78314d96eadcfc7e758a981
Luis Carlos Quinhone <lcquinhone@gmail.com>
2015-05-15 09:46:58 -0400

    Atualização das rotas segundo Feedback

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,29 +15,28 @@ Route::pattern('id', '[0-9]+');
 
 Route::get('/', 'HomeController@index');
 
-/* Rotas para Categorias*/
-Route::group(['prefix' => 'categories'], function(){
-
-	Route::get('/', 			array('as' => 'category_index',	'uses' => 'CategoriesController@index'));
-    Route::get('create',		array('as' => 'category_create', 'uses' => 'CategoriesController@create'));
-    Route::get('delete/{id?}',	array('as' => 'category_delete', 'uses' => 'CategoriesController@delete'));
-
-});
-
-/* Rotas para Categorias*/
-Route::group(['prefix' => 'products'], function(){
-
-	Route::get('/', 			array('as' => 'products_index',	'uses' => 'ProductsController@index'));
-    Route::get('create',		array('as' => 'products_create', 'uses' => 'ProductsController@create'));
-    Route::get('delete/{id?}',	array('as' => 'products_delete', 'uses' => 'ProductsController@delete'));
-
-});
-
 Route::group(['prefix' => 'admin'], function(){
 
 	Route::get('/', 		array('as' => 'admin_index',	'uses' => 'AdminController@index'));
-    Route::get('category',	array('as' => 'admin_category', 'uses' => 'AdminCategoriesController@index'));
-    Route::get('product', 	array('as' => 'admin_product', 	'uses' => 'AdminProductsController@index'));
+
+    /* Rotas para Categorias*/
+    Route::group(['prefix' => 'categories'], function(){
+
+        Route::get('/',             array('as' => 'category_index', 'uses' => 'CategoriesController@index'));
+        Route::get('create',        array('as' => 'category_create', 'uses' => 'CategoriesController@create'));
+        Route::get('delete/{id?}',  array('as' => 'category_delete', 'uses' => 'CategoriesController@delete'));
+
+    });
+
+    /* Rotas para Produtos*/
+    Route::group(['prefix' => 'products'], function(){
+
+        Route::get('/',             array('as' => 'products_index', 'uses' => 'ProductsController@index'));
+        Route::get('create',        array('as' => 'products_create', 'uses' => 'ProductsController@create'));
+        Route::get('delete/{id?}',  array('as' => 'products_delete', 'uses' => 'ProductsController@delete'));
+
+    });
+
 
 });
 
